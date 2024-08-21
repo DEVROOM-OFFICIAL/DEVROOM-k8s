c9467b15715718abbf06f4b75de4b2a779e718eb
Yanghyeondong <hdyang0686@naver.com>
2024-08-21 16:47:12 +0900

    ✨ NFS 미사용 옵션 추가

--- install_k3s_agent.sh
+++ install_k3s_agent.sh
@@ -8,14 +8,23 @@ sudo chmod 755 install_k3s_agent.sh
 
 LINE="===================="
 
-# install nfs
-echo "${LINE} Install nfs... ${LINE}"
-read -p "Enter the nfs address: " mynfs
-sudo apt install -y nfs-common >> install.log 2>&1
-sudo mkdir -p /nfs_devroom >> install.log
-sudo mount ${mynfs} /nfs_devroom >> install.log
-sudo chmod -R 777 /nfs_devroom
-ls -al /nfs_devroom
+# NFS 설치 & 마운트 여부 (단일노드만 사용할 경우 설치 안함)
+read -p "Install NFS? (y/n): " install_nfs
+
+if [[ "$install_nfs" == "y" || "$install_nfs" == "Y" ]]; then
+    echo "${LINE} Install nfs... ${LINE}"
+    read -p "Enter the nfs address: " mynfs
+    sudo apt install -y nfs-common >> install.log 2>&1
+    sudo mkdir -p /nfs_devroom >> install.log
+    sudo mount ${mynfs} /nfs_devroom >> install.log
+    sudo chmod -R 777 /nfs_devroom
+    ls -al /nfs_devroom
+else
+    echo "Using single node storage ..."
+    sudo mkdir -p /nfs_devroom >> install.log
+    sudo chmod -R 777 /nfs_devroom
+    ls -al /nfs_devroom
+fi
 
 echo "${LINE} Install k3s... ${LINE}"
 read -p "Enter the K3S server address: " myserver
